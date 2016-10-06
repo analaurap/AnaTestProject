adf998c4b52564327265f137bd6519a213ac44dd
Ana Laura Pace <analaura.pace@tangoe.com>
2016-10-06 10:20:34 -0300

    Test 2

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Sharing a project using GIT.");
+		System.out.println("Test 2.");
 	}
 
 }
